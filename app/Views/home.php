a7e02901d6c0c40784fdaa5ab05e609936b1400d
NeoTRAN <neo_tran@outlook.com>
2020-06-29 00:09:10 -0500

    test

---
+++ app/Views/home.php
@@ -0,0 +1,68 @@
+
+
+    <div class="fire">
+        <div class="fire-left">
+            <div class="main-fire"></div>
+            <div class="particle-fire"></div>
+        </div>
+        <div class="fire-main">
+            <div class="main-fire"></div>
+            <div class="particle-fire"></div>
+        </div>
+        <div class="fire-right">
+            <div class="main-fire"></div>
+            <div class="particle-fire"></div>
+        </div>
+        <div class="fire-bottom">
+            <div class="main-fire"></div>
+        </div>
+    </div>
+
+    <div class="ui grid" style="padding-top: 6%;">
+        <div class="three wide column"></div>
+        <div class="four wide column">
+            <h1 class="ui header" style="color: white; font-size: 70px;">Scientia</h1>
+        </div>
+        <div class="four wide column">
+            <div class="ui cards">
+                <div class="card">
+                    <div class="content">
+                    <img class="right floated mini ui image" src="/images/avatar/large/elliot.jpg">
+                    <div class="header">Elliot Fu </div>
+                    <div class="meta">Amigos de Veronika </div>
+                    <div class="description">Elliot requested permission to view your contact details </div>
+                    </div>
+                    <div class="extra content">
+                    <div class="ui two buttons">
+                        <div class="ui basic green button">Approve</div>
+                        <div class="ui basic red button">Declinar</div>
+                    </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="content">
+                    <img class="right floated mini ui image" src="/images/avatar/large/jenny.jpg">
+                    <div class="header">Jenny Hess </div>
+                    <div class="meta">Nuevo Miembro </div>
+                    <div class="description">Jenny wants to add you to the group <b>best friends</b> </div>
+                    </div>
+                    <div class="extra content">
+                    <div class="ui two buttons">
+                        <div class="ui basic green button">Approve</div>
+                        <div class="ui basic red button">Declinar</div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="four wide column">
+            <div class="ui labeled button" tabindex="0">
+                <div class="ui red button"><i class="heart icon"></i> Gustan </div>
+                <a class="ui basic red left pointing label">
+                    1,048
+                </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
